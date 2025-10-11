ed551cb204be0bb7d664278083892868f3201dce
CamiBrandolin <cami.brandolin@gmail.com>
2025-10-11 18:35:36 -0300

    segundo fix

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getProductsController } from '../controllers/product.controller.js';
+import { getProductsController , getProductByIdController} from '../controllers/product.controller.js';
 
 const router = Router();
 
